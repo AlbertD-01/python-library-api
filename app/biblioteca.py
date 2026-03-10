fa703c484b00abd0d96c45e5678f566233535c82
Alberto <alberto.d.abut@gmail.com>
2026-03-10 13:31:35 +0100

    feat: Préstamo de libros

--- app/biblioteca.py
+++ app/biblioteca.py
@@ -17,3 +17,14 @@ class Biblioteca:
             if titulo.lower() in libro.titulo.lower()
         ]
         return resultados
+
+    def prestar_libro(self, titulo: str):
+        for libro in self.libros:
+            if libro.titulo.lower() == titulo.lower():
+                if libro.disponible:
+                    libro.disponible = False
+                    return {"exito": True, "mensaje": f"Libro '{libro.titulo}' prestado con éxito."}
+                else:
+                    return {"exito": False, "mensaje": f"El libro '{libro.titulo}' ya está prestado."}
+
+        return {"exito": False, "mensaje": "Libro no encontrado."}
--- app/main.py
+++ app/main.py
@@ -26,3 +26,10 @@ async def crear_libro(nuevo_libro: Libro):
 @app.get("/libros/buscar")
 async def buscar_libro_por_titulo(titulo: str):
     return mi_biblioteca.buscar_por_titulo(titulo)
+
+
+@app.post("/libros/prestar")
+async def prestar_libro(titulo: str):
+    resultado = mi_biblioteca.prestar_libro(titulo)
+
+    return resultado 
