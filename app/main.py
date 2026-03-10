3635293f7b2f13ee3e8352ddf68aaa8b16005e81
Alberto <alberto.d.abut@gmail.com>
2026-03-10 10:39:07 +0100

    feat: Definir modelo de datos python con Pydantic

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
+from libro import Libro
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {
-        "status": "online",
-        "message": "Servicio de Biblioteca funcionando correctamente",
-        "fase": 1
-    }
+    return {"message": "Fase 2 completada: Gestión de libro lista"}
+
+@app.post("/libros/")
+async def crear_libro(libro: Libro):
+    return {"mensaje": "Libro recibido", "datos": libro}
